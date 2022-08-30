8ebb49df0986cc01f6e857aef8047d3967995644
NatalliaMatusevich <natallia.matusevic@gmail.com>
2022-08-30 13:57:47 +0300

    add homework_functions_2.py

---
+++ homework_functions_2.py
@@ -0,0 +1,20 @@
+import random
+
+
+# задача 13.12
+# def bank(n, x):
+#     for i in range(x):
+#         n = n + (n * 10 / 100)
+#     return n
+# print(bank(float(input('Введите сумму: ')), int(input('Введите количество лет: '))))
+
+# задача 13_4
+# def elements(countt, minn, maxx):
+#     ls = []
+#     for i in range(countt):
+#         num = random.randrange(minn, maxx+1)
+#         ls.append(num)
+#
+#     return ls
+#
+# print(elements(int(input('Введите количество элементов: ')), int(input('Введите минимальное значение: ')), int(input('Введите максимальное значение: '))))
