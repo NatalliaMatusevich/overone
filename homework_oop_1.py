5d659dc1f0d08e5fd774965ac28a2d34f30f532c
NatalliaMatusevich <natallia.matusevic@gmail.com>
2022-09-05 18:18:13 +0300

    add homework_oop_2

---
+++ homework_oop_1.py
@@ -0,0 +1,32 @@
+# 14/1
+# class Dog:
+#
+#     def __init__(self, height, weight, name, age):
+#         self.height = height
+#         self.weight = weight
+#         self.name = name
+#         self.age = age
+#
+#     def jump(self):
+#         return print('Прыгай!')
+#
+#     def run(self):
+#         return print('Беги!')
+#
+#     def back(self):
+#         return print('Назад!')
+#
+#     def change_name(self):    # дополнение по задаче 14/2
+#         self.name = input()
+#
+# pug = Dog(2, 4, 'Lucky', 1)
+# pug.jump()
+# pug.run()
+# pug.back()
+# print(pug.__dict__)
+# # дополнение по задаче 14/2
+# pug.change_name()
+# print(pug.name)
+#
+
+
