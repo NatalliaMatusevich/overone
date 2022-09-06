c0fae6b18aa369159a31ce3bc78ec55a77ba8def
NatalliaMatusevich <natallia.matusevic@gmail.com>
2022-09-06 12:11:34 +0300

    add homework_oop_1.py

--- homework_oop_1.py
+++ homework_oop_1.py
@@ -1,32 +1,34 @@
 # 14/1
-# class Dog:
-#
-#     def __init__(self, height, weight, name, age):
-#         self.height = height
-#         self.weight = weight
-#         self.name = name
-#         self.age = age
-#
-#     def jump(self):
-#         return print('Прыгай!')
-#
-#     def run(self):
-#         return print('Беги!')
-#
-#     def back(self):
-#         return print('Назад!')
-#
-#     def change_name(self):    # дополнение по задаче 14/2
-#         self.name = input()
-#
-# pug = Dog(2, 4, 'Lucky', 1)
-# pug.jump()
-# pug.run()
-# pug.back()
-# print(pug.__dict__)
-# # дополнение по задаче 14/2
-# pug.change_name()
-# print(pug.name)
-#
+class Dog:
+
+    def __init__(self, height, weight, name, age):
+        self.height = height
+        self.weight = weight
+        self.name = name
+        self.age = age
+
+    def jump(self):
+        return print('Прыгай!')
+
+    def run(self):
+        return print('Беги!')
+
+    def back(self):
+        return print('Назад!')
+
+    def change_name(self):    # дополнение по задаче 14/2
+        self.name = new_name
+
+
+pug = Dog(2, 4, 'Lucky', 1)
+pug.jump()
+pug.run()
+pug.back()
+print(pug.__dict__)
+# дополнение по задаче 14/2
+new_name = input('Введите новое имя: ')
+pug.change_name()
+print(pug.name)
+
 
 
